c2bc3e54be1e2ad3a874908ac61369fb7a4262a2
ireneguerrero <irene2g222@gmail.com>
2023-04-27 20:33:53 +0200

    a

---
+++ cliente/src/main/java/cliente/Main.java
@@ -0,0 +1,19 @@
+package cliente;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//1 - Coge el proyecto del repositorio y borra el main
+		//2 - Crea un POJO Cliente que tenga las mismas variables internas que columnas tiene la tabla menos contraseña
+		//3 - Aquí en el main, lo primero que se va a hacer es pedir un usuario y una contraseña por teclado
+		//    Cuando tengas ambos datos, usa la función consultar para traer todos los datos del cliente (menos contraseña)
+		//    desde base de datos solo si el email y contraseña que se han puesto son correctos
+		//4 - Si el array que recibes de la función consultar está vacío, quiere decir que el usuario o la contraseña
+		//    no son correctos:
+		//    Vuelve a pedirlos por teclado las veces que haga falta hasta que ponga los datos correctos
+		//    En ese caso, di al usuario ¡Login correcto!, crea una variable de tipo cliente con los datos que te devuelve
+		//    el ArrayListm y usa toString para imprimir el cliente por pantalla (recuerda: el password no tiene que salir aquí)
+
+	}
+
+}
