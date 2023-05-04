6e65c74a25e03069769234d49323558464109530
ireneguerrero <irene2g222@gmail.com>
2023-05-04 19:38:51 +0200

    a

--- apuntes18/src/main/java/apuntes18/Main.java
+++ apuntes18/src/main/java/apuntes18/Main.java
@@ -1,9 +1,27 @@
 package apuntes18;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 //		Prueba IO
+		try {
+			FileWriter escritor=new FileWriter("./prueba.txt");
+			escritor.write("Hola Mundo ");
+			escritor.write("Sigo escribiendo ");
+			escritor.write("Ya voy a parar ");
+			escritor.write("ya está");
+			escritor.flush();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Pulsa intro");
+			sc.nextLine();
+			escritor.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 
 	}
