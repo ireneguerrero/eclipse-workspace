7aaaa436f4957ba96738ce8cc32600b2b4fa28d5
ireneguerrero <irene2g222@gmail.com>
2023-05-04 17:18:48 +0200

    a

--- apuntes17/src/main/java/apuntes17/Main.java
+++ apuntes17/src/main/java/apuntes17/Main.java
@@ -108,6 +108,34 @@ public class Main {
 				break;
 			}
 		} while (opcion != 0);
+		
+//		ArrayList<Object> resultado=null;
+//		do {
+//		System.out.println("Dime tu email");
+//		String email=sc.nextLine();
+//		System.out.println("Dime tu contraseña");
+//		String contraseña=sc.nextLine();
+//		
+//		LinkedHashSet<String> columnas =new LinkedHashSet<String>();
+//		columnas.add("email");
+//		columnas.add("nombre");
+//		columnas.add("telefono");
+//		
+//		HashMap<String,Object> restricciones=new HashMap<String,Object>();
+//		restricciones.put("email", email);
+//		restricciones.put("password", contraseña);
+//		
+//		try {
+//			resultado= DAO.consultar("cliente", columnas, restricciones);
+//			
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		}while(resultado.isEmpty());
+//		System.out.println("¡Login correcto!");
+//		
+//		Cliente cliente=new  Cliente((String)resultado.get(0), (Integer)resultado.get(2), (String)resultado.get(1));
+//		System.out.println(cliente);
 
 	}
 
---
+++ cliente/src/main/java/exceptions/ContraseñaInvalidaException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ContraseñaInvalidaException extends Exception {
+	public ContraseñaInvalidaException(String mensaje) {
+		super(mensaje);
+	}
+
+}
