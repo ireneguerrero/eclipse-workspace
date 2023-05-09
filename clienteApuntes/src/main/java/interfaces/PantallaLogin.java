7017380c33e2c4c84350534c1a0c3dac66cbeca9
ireneguerrero <irene2g222@gmail.com>
2023-05-09 17:40:48 +0200

    a

--- clienteApuntes/src/main/java/interfaces/PantallaLogin.java
+++ clienteApuntes/src/main/java/interfaces/PantallaLogin.java
@@ -9,17 +9,21 @@ import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import java.awt.Color;
 import javax.swing.JPasswordField;
+import javax.swing.JLabel;
 
-public class PantallaLogin extends JPanel{
-	private JTextField textField;
-	private JTextField textField_2;
-	private JTextField textField_4;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-	public PantallaLogin() {
+public class PantallaLogin extends JPanel {
+	private Ventana ventana;
+	private JTextField usuario1;
+	private JTextField usuario2;
+	private JTextField nombre;
+	private JPasswordField password1;
+	private JPasswordField password2;
+
+	public PantallaLogin(Ventana v) {
+		this.ventana = v;
 		setBackground(new Color(135, 206, 250));
 		setLayout(null);
-		
+
 		JButton botonLogin = new JButton("Login");
 		botonLogin.setToolTipText("Pínchame para iniciar sesión");
 		botonLogin.addActionListener(new ActionListener() {
@@ -28,7 +32,7 @@ public class PantallaLogin extends JPanel{
 		});
 		botonLogin.setBounds(69, 354, 119, 43);
 		add(botonLogin);
-		
+
 		JTextArea txtrWenohDia = new JTextArea();
 		txtrWenohDia.setEditable(false);
 		txtrWenohDia.setForeground(Color.WHITE);
@@ -36,18 +40,18 @@ public class PantallaLogin extends JPanel{
 		txtrWenohDia.setText("wenoh dia");
 		txtrWenohDia.setBounds(290, 35, 110, 22);
 		add(txtrWenohDia);
-		
-		textField = new JTextField();
-		textField.setBounds(79, 214, 96, 19);
-		add(textField);
-		textField.setColumns(10);
-		
+
+		usuario1 = new JTextField();
+		usuario1.setBounds(79, 214, 96, 19);
+		add(usuario1);
+		usuario1.setColumns(10);
+
 		JTextArea txtrPaHaseLogin = new JTextArea();
 		txtrPaHaseLogin.setEditable(false);
 		txtrPaHaseLogin.setText("pa hase login");
 		txtrPaHaseLogin.setBounds(79, 108, 137, 22);
 		add(txtrPaHaseLogin);
-		
+
 		JButton botonRegis = new JButton("Registrarce");
 		botonRegis.setToolTipText("Pínchame pa registrarce");
 		botonRegis.addActionListener(new ActionListener() {
@@ -56,70 +60,71 @@ public class PantallaLogin extends JPanel{
 		});
 		botonRegis.setBounds(488, 354, 119, 43);
 		add(botonRegis);
-		
+
 		JTextArea txtrLaSombrita = new JTextArea();
 		txtrLaSombrita.setEditable(false);
 		txtrLaSombrita.setText("la sombrita");
 		txtrLaSombrita.setBounds(504, 108, 137, 22);
 		add(txtrLaSombrita);
-		
+
 		JTextArea txtrPaRegistrarceVe = new JTextArea();
 		txtrPaRegistrarceVe.setEditable(false);
 		txtrPaRegistrarceVe.setText("pa registrarce ve por la sombrita");
 		txtrPaRegistrarceVe.setBounds(199, 67, 332, 22);
 		add(txtrPaRegistrarceVe);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(488, 259, 96, 19);
-		add(textField_2);
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(488, 203, 96, 19);
-		add(textField_4);
-		
+
+		usuario2 = new JTextField();
+		usuario2.setColumns(10);
+		usuario2.setBounds(488, 259, 96, 19);
+		add(usuario2);
+
+		nombre = new JTextField();
+		nombre.setColumns(10);
+		nombre.setBounds(488, 203, 96, 19);
+		add(nombre);
+
 		JTextPane txtpnNombre = new JTextPane();
 		txtpnNombre.setEditable(false);
 		txtpnNombre.setText("nombre");
 		txtpnNombre.setBounds(489, 173, 67, 19);
 		add(txtpnNombre);
-		
+
 		JTextPane txtpnUsuario = new JTextPane();
 		txtpnUsuario.setEditable(false);
 		txtpnUsuario.setText("usuario");
 		txtpnUsuario.setBounds(79, 188, 77, 22);
 		add(txtpnUsuario);
-		
+
 		JTextPane txtpnUsuario_1 = new JTextPane();
 		txtpnUsuario_1.setEditable(false);
 		txtpnUsuario_1.setText("usuario");
 		txtpnUsuario_1.setBounds(488, 227, 77, 22);
 		add(txtpnUsuario_1);
-		
+
 		JTextPane txtpnContrasea = new JTextPane();
 		txtpnContrasea.setEditable(false);
 		txtpnContrasea.setText("contraseña");
 		txtpnContrasea.setBounds(78, 259, 110, 22);
 		add(txtpnContrasea);
-		
+
 		JTextPane txtpnContrasea_1 = new JTextPane();
 		txtpnContrasea_1.setEditable(false);
 		txtpnContrasea_1.setText("contraseña");
 		txtpnContrasea_1.setBounds(488, 290, 96, 22);
 		add(txtpnContrasea_1);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(79, 301, 101, 22);
-		add(passwordField);
-		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(488, 322, 101, 22);
-		add(passwordField_1);
+
+		password1 = new JPasswordField();
+		password1.setBounds(79, 301, 101, 22);
+		add(password1);
+
+		password2 = new JPasswordField();
+		password2.setBounds(488, 322, 101, 22);
+		add(password2);
 	}
+
 	private static class __Tmp {
 		private static void __tmp() {
-			  javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();
+			javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();
 		}
 	}
 }
--- clienteApuntes/src/main/java/interfaces/Ventana.java
+++ clienteApuntes/src/main/java/interfaces/Ventana.java
@@ -9,8 +9,16 @@ public class Ventana extends JFrame {
 		this.setTitle("Programa de prueba de clase"); // título de la ventana
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE); // si se cierra la ventana termina el programa
 		this.setLocationRelativeTo(null); // se pone la ventana centrada
-		this.setContentPane(new PantallaLogin());
+		this.setContentPane(new PantallaLogin(this));
 		this.setVisible(true); // tiene que ser la última línea del constructor, se hace visible la ventana
+	}
+
+	public void cambiarAPantalla(Class<?> clase) {
+		this.getContentPane().setVisible(false);
+		if (clase.equals(PantallaLogin.class)) {
+			this.setContentPane(new PantallaLogin(this));
+		}
+		this.getContentPane().setVisible(true);
 	}
 
 }
