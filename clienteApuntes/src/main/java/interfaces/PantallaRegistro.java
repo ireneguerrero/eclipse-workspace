3cb4a4c8c55e1c2d1fa04a35f16fd2284c4889ab
ireneguerrero <irene2g222@gmail.com>
2023-05-16 17:16:52 +0200

    a

--- clienteApuntes/src/main/java/interfaces/PantallaRegistro.java
+++ clienteApuntes/src/main/java/interfaces/PantallaRegistro.java
@@ -45,12 +45,13 @@ public class PantallaRegistro extends JPanel {
 	public PantallaRegistro(Ventana v) {
 		this.ventana = v;
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 71, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{57, 0, 29, 32, 45, 43, 21, 39, 24, -18, 16, 0, 27, 40, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 71, 0, 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 57, 0, 29, 32, 45, 43, 21, 39, 24, -18, 16, 0, 27, 40, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		labelTitulo = new JLabel("Regístrate");
 		labelTitulo.setFont(new Font("Arial", Font.BOLD, 42));
 		GridBagConstraints gbc_labelTitulo = new GridBagConstraints();
@@ -60,7 +61,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_labelTitulo.gridx = 1;
 		gbc_labelTitulo.gridy = 0;
 		add(labelTitulo, gbc_labelTitulo);
-		
+
 		labelEmail = new JLabel("Email");
 		GridBagConstraints gbc_labelEmail = new GridBagConstraints();
 		gbc_labelEmail.fill = GridBagConstraints.VERTICAL;
@@ -69,7 +70,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_labelEmail.gridx = 1;
 		gbc_labelEmail.gridy = 2;
 		add(labelEmail, gbc_labelEmail);
-		
+
 		campoEmail = new JTextField();
 		campoEmail.setColumns(10);
 		GridBagConstraints gbc_campoEmail = new GridBagConstraints();
@@ -79,7 +80,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_campoEmail.gridx = 2;
 		gbc_campoEmail.gridy = 2;
 		add(campoEmail, gbc_campoEmail);
-		
+
 		labelNombre = new JLabel("Nombre");
 		GridBagConstraints gbc_labelNombre = new GridBagConstraints();
 		gbc_labelNombre.fill = GridBagConstraints.VERTICAL;
@@ -88,7 +89,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_labelNombre.gridx = 1;
 		gbc_labelNombre.gridy = 3;
 		add(labelNombre, gbc_labelNombre);
-		
+
 		campoNombre = new JTextField();
 		GridBagConstraints gbc_campoNombre = new GridBagConstraints();
 		gbc_campoNombre.gridwidth = 2;
@@ -98,7 +99,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_campoNombre.gridy = 3;
 		add(campoNombre, gbc_campoNombre);
 		campoNombre.setColumns(10);
-		
+
 		labelTelefono = new JLabel("Teléfono");
 		GridBagConstraints gbc_labelTelefono = new GridBagConstraints();
 		gbc_labelTelefono.anchor = GridBagConstraints.WEST;
@@ -106,7 +107,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_labelTelefono.gridx = 1;
 		gbc_labelTelefono.gridy = 4;
 		add(labelTelefono, gbc_labelTelefono);
-		
+
 		campoTelefono = new JTextField();
 		GridBagConstraints gbc_campoTelefono = new GridBagConstraints();
 		gbc_campoTelefono.gridwidth = 2;
@@ -116,7 +117,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_campoTelefono.gridy = 4;
 		add(campoTelefono, gbc_campoTelefono);
 		campoTelefono.setColumns(10);
-		
+
 		labelContraseña = new JLabel("Contraseña");
 		GridBagConstraints gbc_labelContraseña = new GridBagConstraints();
 		gbc_labelContraseña.anchor = GridBagConstraints.WEST;
@@ -124,7 +125,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_labelContraseña.gridx = 1;
 		gbc_labelContraseña.gridy = 5;
 		add(labelContraseña, gbc_labelContraseña);
-		
+
 		campoContraseña = new JPasswordField();
 		campoContraseña.setText("");
 		GridBagConstraints gbc_campoContraseña = new GridBagConstraints();
@@ -134,7 +135,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_campoContraseña.gridx = 2;
 		gbc_campoContraseña.gridy = 5;
 		add(campoContraseña, gbc_campoContraseña);
-		
+
 		labeslGenero = new JLabel("Género");
 		GridBagConstraints gbc_labeslGenero = new GridBagConstraints();
 		gbc_labeslGenero.gridwidth = 3;
@@ -142,7 +143,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_labeslGenero.gridx = 1;
 		gbc_labeslGenero.gridy = 6;
 		add(labeslGenero, gbc_labeslGenero);
-		
+
 		radioHombre = new JRadioButton("Hombre");
 		buttonGroup.add(radioHombre);
 		GridBagConstraints gbc_radioHombre = new GridBagConstraints();
@@ -150,7 +151,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_radioHombre.gridx = 1;
 		gbc_radioHombre.gridy = 7;
 		add(radioHombre, gbc_radioHombre);
-		
+
 		radioMujer = new JRadioButton("Mujer");
 		buttonGroup.add(radioMujer);
 		GridBagConstraints gbc_radioMujer = new GridBagConstraints();
@@ -158,7 +159,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_radioMujer.gridx = 2;
 		gbc_radioMujer.gridy = 7;
 		add(radioMujer, gbc_radioMujer);
-		
+
 		radioOtro = new JRadioButton("Otro");
 		buttonGroup.add(radioOtro);
 		GridBagConstraints gbc_radioOtro = new GridBagConstraints();
@@ -166,21 +167,36 @@ public class PantallaRegistro extends JPanel {
 		gbc_radioOtro.gridx = 3;
 		gbc_radioOtro.gridy = 7;
 		add(radioOtro, gbc_radioOtro);
-		
+
 		botonLimpiar = new JButton("Limpiar formulario");
-		botonLimpiar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		botonLimpiar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				campoEmail.setText("");
+				campoNombre.setText("");
+				campoTelefono.setText("");
+				campoContraseña.setText("");
+				buttonGroup.clearSelection();
 			}
 		});
-		
+
 		botonRegistrar = new JButton("Regístrate");
+		botonRegistrar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String email = campoEmail.getText();
+				String nombre = campoNombre.getText();
+				int telefono = Integer.parseInt(campoTelefono.getText());
+				String contraseña = new String(campoContraseña.getPassword());
+//				if(radioHombre.isSelected()) {
+//					
+//				}//lo mismo con radioMujer y radioOtro
+			}
+		});
 		botonRegistrar.setBackground(new Color(0, 0, 102));
 		botonRegistrar.setFont(new Font("Arial", Font.ITALIC, 18));
 		botonRegistrar.setForeground(Color.WHITE);
-		botonRegistrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
+
 		GridBagConstraints gbc_botonRegistrar = new GridBagConstraints();
 		gbc_botonRegistrar.gridwidth = 3;
 		gbc_botonRegistrar.fill = GridBagConstraints.HORIZONTAL;
@@ -195,7 +211,7 @@ public class PantallaRegistro extends JPanel {
 		gbc_botonLimpiar.gridx = 1;
 		gbc_botonLimpiar.gridy = 10;
 		add(botonLimpiar, gbc_botonLimpiar);
-		
+
 		botonAtras = new JButton("Cancelar");
 		botonAtras.addMouseListener(new MouseAdapter() {
 			@Override
