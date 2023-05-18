c28b436b1e7f2fb815b7e2eb742f61948bcfa4ad
ireneguerrero <irene2g222@gmail.com>
2023-05-18 19:30:10 +0200

    a

--- labyrinth/src/main/java/clases/Laberinto.java
+++ labyrinth/src/main/java/clases/Laberinto.java
@@ -13,6 +13,60 @@ public class Laberinto extends ElementoConNombreEImagen {
 		this.celdas = celdas;
 		this.nivel = nivel;
 	}
+	
+	public Laberinto(int numero) {
+	    super("Laberinto", null);
+	    this.nivel = (byte) (numero % 10); // Obtener el último dígito del número
+
+//	    Este constructor crea un laberinto en función del número proporcionado.
+//	    Se utiliza un switch para asignar diferentes valores a la matriz de celdas según el número.
+//	    Si el número no coincide con ninguno de los casos especificados,
+//	    se crea un laberinto vacío con un solo elemento en la matriz de celdas y el nivel se establece en 0.
+	    
+	    switch (numero) {
+	        case 1:
+	            this.celdas = new byte[][]{
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 0, 0, 1},
+	                    {1, 1, 1, 0, 1},
+	                    {1, 0, 0, 0, 1},
+	                    {1, 1, 1, 1, 1}
+	            };
+	            break;
+	        case 2:
+	            this.celdas = new byte[][]{
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 0, 0, 1},
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 0, 0, 1},
+	                    {1, 1, 1, 1, 1}
+	            };
+	            break;
+	        case 3:
+	            this.celdas = new byte[][]{
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 1, 0, 1},
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 0, 0, 1},
+	                    {1, 1, 1, 1, 1}
+	            };
+	            break;
+	        case 4:
+	            this.celdas = new byte[][]{
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 1, 0, 1},
+	                    {1, 1, 1, 1, 1},
+	                    {1, 0, 1, 0, 1},
+	                    {1, 1, 1, 1, 1}
+	            };
+	            break;
+	        default:
+	            this.celdas = new byte[][]{{0}};
+	            this.nivel = 0;
+	            break;
+	    }
+	}
+
 
 	public byte[][] getCeldas() {
 		return celdas;
