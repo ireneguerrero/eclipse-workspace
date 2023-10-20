82599757bd9d4ea419c28bb35aa257c08443b62a
ireneguerrero <irene2g222@gmail.com>
2023-10-20 12:38:24 +0200

    a

---
+++ conectarmysql/src/main/java/conectarmysql/ConectarDb.java
@@ -0,0 +1,24 @@
+package conectarmysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectarDb {
+
+	public static void main(String[] args) {
+		Connection conect = null;
+
+		try {
+			conect = DriverManager.getConnection("jdbc:mysql://localhost/AAD1", "root", "");
+			if (conect != null) {
+				System.out.println("Conexión a base de datos ok");
+				conect.close();
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+}
