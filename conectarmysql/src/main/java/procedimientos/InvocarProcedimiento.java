a4c7a8a12ba84af63161337289e98b56f4e490fc
ireneguerrero <irene2g222@gmail.com>
2023-11-03 14:25:35 +0100

    a

--- conectarmysql/src/main/java/procedimientos/InvocarProcedimiento.java
+++ conectarmysql/src/main/java/procedimientos/InvocarProcedimiento.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 
 public class InvocarProcedimiento {
 
@@ -13,20 +14,25 @@ public class InvocarProcedimiento {
 		CallableStatement cstmt = null;
 
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/empresa", "root", "");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/phoneland", "root", "");
 //			cstmt = conn.prepareCall("call ObtenerClientes()");
-			cstmt = conn.prepareCall("call CalcularEdad()");
+			cstmt = conn.prepareCall("call CalcularEdad(?,?)");
+			cstmt.setInt(1, 2000);
+			cstmt.registerOutParameter(2, Types.INTEGER);
 			cstmt.execute();
 
-			ResultSet rs = cstmt.getResultSet();
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				String nif = rs.getString("nif");
-				String nombre = rs.getString("nombre");
-				String edad = rs.getString("edad");
-				System.out.println("id: " + id + ", nif: " + nif + ", nombre: " + nombre + ", edad: " + edad);
-			}
+//			ResultSet rs = cstmt.getResultSet();
+//			while (rs.next()) {
+//				int id = rs.getInt("id");
+//				String nif = rs.getString("nif");
+//				String nombre = rs.getString("nombre");
+//				String edad = rs.getString("edad");
+//				System.out.println("id: " + id + ", nif: " + nif + ", nombre: " + nombre + ", edad: " + edad);
+//			}
+
+			int edad = cstmt.getInt(2);
 			System.out.println("Procedimiento invocado");
+			System.out.println("La edad es " + edad);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
