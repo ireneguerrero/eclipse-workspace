c1df7621770308e6963e14400e4dcc5844966926
ireneguerrero <irene2g222@gmail.com>
2023-11-06 12:36:48 +0100

    a

---
+++ confeccionInterfaces/src/main/java/confeccionInterfaces/ContenedorMensaje.java
@@ -0,0 +1,58 @@
+package confeccionInterfaces;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class ContenedorMensaje extends JFrame {
+	JLabel etiquetaMensaje;
+
+	public ContenedorMensaje() {
+		setTitle("Ejemplo con dos contenedores");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Creamos los contenedores
+		JPanel panelBotones = new JPanel();
+		JPanel panelMensaje = new JPanel();
+
+		// El mensaje que aparece en la ventana
+		etiquetaMensaje = new JLabel("Mensaje: ");
+
+		// Bucle donde creamos los diferentes botones
+		for (int i = 1; i <= 3; i++) {
+			JButton boton = new JButton("Botón " + i);
+			// Le añadimos el listener para cada boton
+			boton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					// Extraemos el nombre del boton
+					String textoBoton = ((JButton) e.getSource()).getText();
+					etiquetaMensaje.setText("Mensaje: " + textoBoton);
+				}
+			});
+
+			panelBotones.add(boton);
+		}
+		setLayout(new BorderLayout());
+		add(panelBotones, BorderLayout.SOUTH);
+		add(panelMensaje, BorderLayout.CENTER);
+		panelMensaje.add(etiquetaMensaje);
+
+		pack(); // Ajusta el tamaño de los elementos del JFrame
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new ContenedorMensaje();
+			}
+		});
+	}
+
+}
