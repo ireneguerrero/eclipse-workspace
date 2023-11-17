50db6ece133bb2479fb54678cd392c98f8520664
ireneguerrero <irene2g222@gmail.com>
2023-11-17 09:37:34 +0100

    a

--- simulacroAccesoADatos/src/main/java/simulacro/SimulacroConectarDb.java
+++ simulacroAccesoADatos/src/main/java/simulacro/SimulacroConectarDb.java
@@ -1,9 +1,23 @@
 package simulacro;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SimulacroConectarDb {
 
 	public static void main(String[] args) {
-		
+		Connection conect = null;
+
+		try {
+			conect = DriverManager.getConnection("jdbc:mysql://localhost/AAD1", "root", "");
+			if (conect != null) {
+				System.out.println("Conexión a base de datos ok");
+				conect.close();
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 
 	}
 
