4b0c74fe338ffc4c47de89e66ecca0b45103a7cb
ireneguerrero <irene2g222@gmail.com>
2024-02-01 11:00:00 +0100

    a

---
+++ plsql/src/main/java/plsql/Ejercicio4.java
@@ -0,0 +1,53 @@
+package plsql;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        // Configura la sesión de Hibernate
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+
+        try (Session session = sessionFactory.openSession()) {
+            // Usa Session.doWork para ejecutar SQL nativo
+            session.doWork(connection -> {
+                // Configura la llamada al procedimiento almacenado
+                String procedimientoAlmacenado = "{ CALL ejecutar_ejercicio4(?) }";
+
+
+                // Ejecuta el procedimiento almacenado
+                try (CallableStatement statement = connection.prepareCall(procedimientoAlmacenado)) {
+                    // Registra el parámetro de salida
+                    statement.registerOutParameter(1, Types.VARCHAR);
+
+
+                    // Ejecuta el procedimiento almacenado
+                    statement.execute();
+
+
+                    // Obtiene el valor del parámetro de salida
+                    String output = statement.getString(1);
+
+
+                    // Muestra la salida
+                    System.out.println(output);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Cierra la sesión de Hibernate al finalizar
+            sessionFactory.close();
+        }
+    }
+}
