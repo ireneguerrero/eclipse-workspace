b1fae02f0595825cf9612e8b5c70f568bfa39760
ireneguerrero <irene2g222@gmail.com>
2024-02-05 10:59:54 +0100

    a

---
+++ tema5interfacesAvanzadosYCSS/src/module-info.java
@@ -0,0 +1,12 @@
+module tema5interfacesAvanzadosYCSS {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens ej1 to javafx.graphics, javafx.fxml;
+	opens ej2 to javafx.graphics, javafx.fxml;
+	opens calculadora to javafx.graphics, javafx.fxml;
+	opens menu to javafx.graphics, javafx.fxml;
+	opens hilo to javafx.graphics, javafx.fxml;
+	opens progressSlider to javafx.graphics, javafx.fxml;
+}
