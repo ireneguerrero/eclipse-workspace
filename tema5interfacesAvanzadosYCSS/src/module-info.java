03ed104a447b7549268d629a835be88a2e051bd9
ireneguerrero <irene2g222@gmail.com>
2024-02-05 11:05:54 +0100

    a

--- tema5interfacesAvanzadosYCSS/src/module-info.java
+++ tema5interfacesAvanzadosYCSS/src/module-info.java
@@ -1,6 +1,8 @@
 module tema5interfacesAvanzadosYCSS {
 	requires javafx.controls;
+	requires javafx.graphics;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens ej1 to javafx.graphics, javafx.fxml;
