22fe6888068c2b90ae3cec71d7d1e9e7a9d2b146
ireneguerrero <irene2g222@gmail.com>
2024-02-12 18:10:35 +0100

    a

---
+++ javafxBBDD/src/module-info.java
@@ -0,0 +1,11 @@
+module javafxBBDD {
+	requires javafx.controls;
+	requires java.sql;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens ejercicio1 to javafx.graphics, javafx.fxml;
+	opens ejercicio2 to javafx.graphics, javafx.fxml;
+	opens ejercicio3 to javafx.graphics, javafx.fxml;
+	opens ejercicio4 to javafx.graphics, javafx.fxml;
+}
